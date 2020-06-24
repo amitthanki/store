56e588bed1f8936ba355fca75499bb2cda7f3510
Akram El Assas <akram.elassas@gmail.com>
2020-06-24 16:21:14 +0100

    Add Catalog Swagger UI

--- src/microservices/CatalogMicroservice/Startup.cs
+++ src/microservices/CatalogMicroservice/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using Middleware;
 
 namespace CatalogMicroservice
@@ -21,6 +22,10 @@ namespace CatalogMicroservice
         {
             services.AddControllers();
             services.AddMongoDb(Configuration);
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Catalog", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,6 +35,13 @@ namespace CatalogMicroservice
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Catalog V1");
+            });
 
             app.UseHttpsRedirection();
 
