f732caefeb3be334520d73bfc6f7ec3877304b4b
Akram El Assas <akram.elassas@gmail.com>
2020-06-24 16:29:28 +0100

    Add Identity Swagger UI

--- src/microservices/IdentityMicroservice/Startup.cs
+++ src/microservices/IdentityMicroservice/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using Middleware;
 
 namespace IdentityMicroservice
@@ -23,6 +24,10 @@ namespace IdentityMicroservice
             services.AddMongoDb(Configuration);
             services.AddJwt(Configuration);
             services.AddTransient<IEncryptor, Encryptor>();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Identity", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -32,6 +37,13 @@ namespace IdentityMicroservice
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Identity V1");
+            });
 
             app.UseHttpsRedirection();
 
