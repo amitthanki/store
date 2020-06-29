de609a6a49e256cab1fdc2b9ad7e4d14baab772b
Akram El Assas <akram.elassas@gmail.com>
2020-06-29 11:56:30 +0100

    Create health checks in cart microservice

--- src/microservices/CartMicroservice/Startup.cs
+++ src/microservices/CartMicroservice/Startup.cs
@@ -1,8 +1,11 @@
 using CartMicroservice.Repository;
+using HealthChecks.UI.Client;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using Middleware;
@@ -29,6 +32,13 @@ namespace CartMicroservice
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Cart", Version = "v1" });
             });
+            services.AddHealthChecks()
+                .AddMongoDb(
+                mongodbConnectionString: Configuration.GetSection("mongo").Get<MongoOptions>().ConnectionString,
+                name: "mongo",
+                failureStatus: HealthStatus.Unhealthy
+                );
+            services.AddHealthChecksUI().AddInMemoryStorage();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,6 +48,14 @@ namespace CartMicroservice
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseHealthChecks("/healthz", new HealthCheckOptions()
+            {
+                Predicate = _ => true,
+                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+            });
+
+            app.UseHealthChecksUI();
 
             app.UseSwagger();
 
